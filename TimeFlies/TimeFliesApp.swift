7f5aaf5b2c0691001d76841023eabb553f10a71d
Aleksander Stojanowski <root.zelatyna.0f@icloud.com>
2023-02-20 21:20:24 +0100

    Initial commit

---
+++ TimeFlies/TimeFliesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeFliesApp.swift
+//  TimeFlies
+//
+//  Created by ostojan on 20/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct TimeFliesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
