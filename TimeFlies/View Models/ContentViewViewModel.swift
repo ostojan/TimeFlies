5c04162e59ba6d94faaa92aeb4f4f5b401e75ead
Aleksander Stojanowski <root.zelatyna.0f@icloud.com>
2023-02-20 22:48:20 +0100

    ContentViewViewModel

---
+++ TimeFlies/View Models/ContentViewViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  ContentViewViewModel.swift
+//  TimeFlies
+//
+//  Created by ostojan on 20/02/2023.
+//
+
+import Foundation
+
+class ContentViewViewModel: ObservableObject {
+    private let dataManager: any TimeFliesDataManaging
+    @Published private(set) var events: [Event] = []
+
+    init(dataManager: any TimeFliesDataManaging) {
+        self.dataManager = dataManager
+    }
+
+    func loadAllEvents() async {
+        do {
+            events = try await dataManager.readAll()
+        } catch {
+            fatalError("Cannot load events")
+        }
+    }
+}
