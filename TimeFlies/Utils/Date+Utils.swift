48353113f3ec73847a53dea184416111baba6a94
Aleksander Stojanowski <root.zelatyna.0f@icloud.com>
2023-02-24 12:21:44 +0100

    Add more utils to Date extension

--- TimeFlies/Utils/Date+Utils.swift
+++ TimeFlies/Utils/Date+Utils.swift
@@ -1,5 +1,5 @@
 //
-//  Date+StartOfDay.swift
+//  Date+Utils.swift
 //  TimeFlies
 //
 //  Created by ostojan on 21/02/2023.
@@ -10,5 +10,9 @@ import Foundation
 extension Date {
     var startOfDay: Date {
         Calendar.current.startOfDay(for: self)
+    }
+
+    func numDaysTo(_ date: Date) -> Int? {
+        Calendar.current.dateComponents([.day], from: self, to: date).day
     }
 }
