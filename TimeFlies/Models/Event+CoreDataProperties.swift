d506c5f7085faeb9d60782decb3a922c01f5ce64
Aleksander Stojanowski <root.zelatyna.0f@icloud.com>
2023-03-05 16:34:04 +0100

    Add RGB components to Event model

--- TimeFlies/Models/Event+CoreDataProperties.swift
+++ TimeFlies/Models/Event+CoreDataProperties.swift
@@ -8,6 +8,7 @@
 
 import CoreData
 import Foundation
+import SwiftUI
 
 public extension Event {
     @nonobjc class func fetchRequest() -> NSFetchRequest<Event> {
@@ -17,6 +18,10 @@ public extension Event {
     @NSManaged var name: String?
     @NSManaged var date: Date?
     @NSManaged var notes: String?
+    // TODO: Use better way to store Event color
+    @NSManaged private var red: Double
+    @NSManaged private var green: Double
+    @NSManaged private var blue: Double
 
     var wrappedName: String {
         name ?? "Unknown Name"
@@ -28,6 +33,18 @@ public extension Event {
 
     var wrappedNotes: String {
         notes ?? ""
+    }
+
+    var color: Color {
+        get {
+            Color(red: red, green: green, blue: blue)
+        }
+        set {
+            let components = newValue.components()
+            red = components.red
+            green = components.green
+            blue = components.blue
+        }
     }
 }
 
