9872c41ac056ff2cbb3339aeff28c938e5e3be42
Aleksander Stojanowski <root.zelatyna.0f@icloud.com>
2023-03-08 22:48:27 +0100

    Add additional Text with time since event to EventDetailsView

--- TimeFlies/View Models/EventDetailsViewViewModel.swift
+++ TimeFlies/View Models/EventDetailsViewViewModel.swift
@@ -20,11 +20,43 @@ class EventDetailsViewViewModel: ObservableObject {
     }
 
     var dateLead: String {
+        // TODO: Handle future events
         "Took Place"
     }
 
     var dateFormatted: String {
         event.wrappedDate.formatted(date: .complete, time: .omitted)
+    }
+
+    var timeLead: String {
+        "Which is"
+    }
+
+    var time: String {
+        // TODO: Handle future events
+        let components = event.wrappedDate.yearMonthAndDayComponentsTo(Date.now.startOfDay)
+        guard let days = components.day,
+              let months = components.month,
+              let years = components.year
+        else {
+            fatalError()
+        }
+        var resultString = ""
+
+        if years > 0 {
+            resultString += "\(years) "
+            resultString += years == 1 ? "year, " : "years, "
+        }
+
+        if months > 0 {
+            resultString += "\(months) "
+            resultString += months == 1 ? "month, " : "months, "
+        }
+
+        resultString += "\(days) "
+        resultString += days == 1 ? "day " : "days "
+        resultString += "ago"
+        return resultString.trimmingCharacters(in: .whitespacesAndNewlines)
     }
 
     var notesLead: String {
